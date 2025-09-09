80f73c88dd64ef118786d7c649b2a5f0363fde03
tanpham253 <85584571+tanpham253@users.noreply.github.com>
2025-09-09 17:05:38 +0700

    alias

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blogpage = () => {
+  return (
+    <div>Blog</div>
+  )
+}
+
+export default Blogpage
---
+++ app/customer/profile/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pageProfile = () => {
+  return (
+    <div>profilepage</div>
+  )
+}
+
+export default pageProfile
